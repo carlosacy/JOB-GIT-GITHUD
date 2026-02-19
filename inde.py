48dd3a9f52c75bbfc890bcd5a6abc63619db816a
carlosacy <ingciry01@gmail.com>
2026-02-19 12:55:59 -0500

    multiplicacion de elemento lista

--- inde.py
+++ inde.py
@@ -5,3 +5,9 @@ suma=0
 for i in range(len(lista)):
     suma+=lista[i]
 print(f'n\la suma de los elementos de la lista es: {suma}')
+
+
+multiplicacion=1
+for i in range(len(lista)):
+    multiplicacion*=lista[i]
+print(f'n\la multiplicacion de los elementos de la lista es: {multiplicacion}')
