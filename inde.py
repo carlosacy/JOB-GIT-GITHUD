92a9e8cf33fd4552e16b9476d998bb652ce88039
carlosacy <ingciry01@gmail.com>
2026-02-19 12:53:44 -0500

    suma elementos de la listas

--- inde.py
+++ inde.py
@@ -1,2 +1,7 @@
 lista=[1,2,3,4,5,9,89]
 print(f'n\la longitud de la lista es: {len(lista)}')
+
+suma=0
+for i in range(len(lista)):
+    suma+=lista[i]
+print(f'n\la suma de los elementos de la lista es: {suma}')
